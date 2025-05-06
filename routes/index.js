545e54cecd31f2ed7e2e16450aa36cf01811cfa5
Choudhary Gouspasha <150334833+Gous3007@users.noreply.github.com>
2025-05-06 17:14:19 +0530

    Production Mode

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
-var express = require('express');
-var router = express.Router();
+let express = require('express');
+let router = express.Router();
+let axios = require("axios");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res) => {
+  res.render("index");
+});
+
+router.get('/download-resume', async (req, res) => {
+  try {
+    const fileUrl = 'https://res.cloudinary.com/drppaqhmd/image/upload/v1746122160/resumes/resume-Gaus_Choudhari-1746122158828-713735502.pdf';
+    const fileName = 'Gaus_Resume.pdf';
+
+    const response = await axios.get(fileUrl, { responseType: 'stream' });
+
+    res.setHeader('Content-Disposition', `attachment; filename="${fileName}"`);
+    response.data.pipe(res);
+  } catch (error) {
+    res.status(500).send('Error downloading resume');
+  }
 });
 
 module.exports = router;
